7ef9102558d269b462e7a052666728cfa7e256a4
ohletamyleta <ohletamyleta@gmail.com>
2021-01-25 21:49:46 -0500

    commit tree c6b6dd4944919592ea5c0fc514677693437ab7b7

--- app.rb
+++ app.rb
@@ -3,7 +3,26 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
-  end
+    get '/new' do
+      erb :'pirates/new'
+    end
+ 
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+     
+
+      params[:pirate][:ship].each do |params|
+        Ship.new(params)
+      end
+     
+      @ships = Ship.all
+      binding.pry
+
+      erb :'pirates/show'
+    end
+end 
 end
